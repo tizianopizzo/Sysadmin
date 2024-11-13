00c56a4bb7787b58447ab18a9c21f527bb189e61
tizianopizzo <100539185+tizianopizzo@users.noreply.github.com>
2024-11-13 11:30:27 +0100

    Add files via upload

---
+++ Script/Linux/Comandi utili dischi.sh
@@ -0,0 +1,5 @@
+#Per vedere la situazione dischi
+df -h
+
+#Per vedere quanto pesano le cartelle
+ncdu
---
+++ Script/Linux/Comandi utili rete.sh
@@ -0,0 +1,7 @@
+#Per vedere MAC address scheda di rete
+/sbin/ifconfig
+
+#Per vedere DNS
+cat /etc/resolv.conf
+#oppure
+systemd-resolve —status
